7dca41312441ef8774a29ba0b76509912f483972
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2020-06-24 21:14:58 +0530

    Add files via upload

--- farms/admin.py
+++ farms/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Division, Farm
 
 # Register your models here.
+
+
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = (
+        "division_name",
+        "division_code",
+    )
+
+
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(Farm)
